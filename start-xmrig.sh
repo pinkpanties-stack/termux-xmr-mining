c4ff7dccc9b93f5d9956d113063f39eef9f496b0
Doctor Who <jiab77@pm.me>
2024-04-02 22:15:02 +0200

    Update start-xmrig.sh

--- start-xmrig.sh
+++ start-xmrig.sh
@@ -6,15 +6,22 @@ set -o xtrace
 # Config
 LIMIT_ON_BATTERY=true
 ENABLE_OPENCL=true
+SLOW_MODE=false
+SLOW_MODE_THRES=4G
 
 # Internals
 BIN_XMRIG=~/xmrig/build/xmrig
+SLOW_MODE_THRES_CLEAN="${SLOW_MODE_THRES//G/}"
+SLOW_MODE_RAM_SIZE=$((SLOW_MODE_THRES_CLEAN*1024*1024))
+DEVICE_MEM_SIZE=$(cat /proc/meminfo | cut -d" " -f9)
 
 # Checks
-[[ -z $($BIN_XMRIG --print-platforms 2>/dev/null) ]] && ENABLE_OPENCL=false
+[[ $DEVICE_MEM_SIZE -lt $SLOW_MODE_RAM_SIZE ]] && SLOW_MODE=true
+[[ $SLOW_MODE == true || -z $($BIN_XMRIG --print-platforms 2>/dev/null) ]] && ENABLE_OPENCL=false
 
 # Init
 [[ $ENABLE_OPENCL == true ]] && OPT_ARGS="--opencl --opencl-platform=0"
+[[ $SLOW_MODE == true ]] && OPT_ARGS="--randomx-mode=light"
 
 # Main
 if [[ $LIMIT_ON_BATTERY == false ]]; then
